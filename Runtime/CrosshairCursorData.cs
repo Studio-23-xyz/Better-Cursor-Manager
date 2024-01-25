e2c9bd3a657bcaa22eeb71cd4311c566273f523c
Zubair Islam Epty <33396274+zubEpty@users.noreply.github.com>
2024-01-25 18:14:28 +0600

    Merge pull request #6 from Studio-23-xyz/Dev
    1.0.10

---
+++ Runtime/CrosshairCursorData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+namespace Studio23.SS2.BetterCursorManager.Data
+{
+    public class CrosshairCursorData : ScriptableObject
+    {
+        public Sprite CrosshairSprite;
+    }
+}
