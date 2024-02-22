805a59b705387817dc41a7acf09a7644c58b50b6
Zubair Islam Epty <eptyt4@gmail.com>
2024-02-22 16:50:57 +0600

    gamepad cursor

--- Assets/Packages/com.studio23.ss2.bettercursormanager/Runtime/Core/Controllers/CursorLocoMotionController.cs
+++ Assets/Packages/com.studio23.ss2.bettercursormanager/Runtime/Core/Controllers/CursorLocoMotionController.cs
@@ -1,6 +1,7 @@
 using Studio23.SS2.BetterCursor.Data;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
 
 namespace Studio23.SS2.BetterCursor.Core
 {
@@ -9,6 +10,12 @@ namespace Studio23.SS2.BetterCursor.Core
         [SerializeField] private RectTransform _cursorTransoform;
         [SerializeField] private Canvas _canvas;
         public InputActionAsset CursorActionAsset;
+        private Vector2 screenBounds;
+
+        private Vector2 minScreenBounds;
+        private Vector2 maxScreenBounds;
+        private bool isDragging = false;
+
 
         private void Awake()
         {
@@ -22,19 +29,53 @@ namespace Studio23.SS2.BetterCursor.Core
             _cursorTransoform.pivot = cursorData.HotSpot;
         }
 
+        void Start()
+        {
+            Canvas canvas = _cursorTransoform.GetComponentInParent<Canvas>();
+            float screenWidth = Screen.width;
+            float screenHeight = Screen.height;
+            Vector2 screenSizeInCanvas = new Vector2(screenWidth, screenHeight) / canvas.scaleFactor;
+            Vector2 uiElementSize = _cursorTransoform.sizeDelta / 2.0f;
+            minScreenBounds = new Vector2(uiElementSize.x / 2, uiElementSize.y / 2);
+            maxScreenBounds = screenSizeInCanvas;
+        }
+
         private void Update()
         {
-            UpdateCursorPosition();
+            //UpdateControllerInput();
+            //HandleMouseInput();
+            var cursorPosition = CursorActionAsset["CursorPosition"].ReadValue<Vector2>();
+            Debug.Log($"Input data {cursorPosition}");
         }
 
 
-        private void UpdateCursorPosition()
+        private void UpdateControllerInput()
         {
-            if (CursorActionAsset != null)
+            Vector2 stickInput = Gamepad.current?.leftStick.ReadValue() ?? Vector2.zero;
+            Vector3 move = new Vector3(stickInput.x, stickInput.y, 0) * 1000f * Time.deltaTime;
+
+            Vector2 nextPosition = _cursorTransoform.anchoredPosition + new Vector2(move.x, move.y);
+            nextPosition.x = Mathf.Clamp(nextPosition.x, minScreenBounds.x, maxScreenBounds.x);
+            nextPosition.y = Mathf.Clamp(nextPosition.y, minScreenBounds.y, maxScreenBounds.y);
+            _cursorTransoform.anchoredPosition = nextPosition;
+            Debug.Log("");
+        }
+
+        void HandleMouseInput()
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame)
             {
-                if (_canvas == null) return;
-                if (Cursor.lockState == CursorLockMode.Locked) return;
+                isDragging = true;
+            }
+            if (Mouse.current.leftButton.wasReleasedThisFrame)
+            {
+                isDragging = false;
+            }
+            if (isDragging)
+            {
                 var cursorPosition = CursorActionAsset["CursorPosition"].ReadValue<Vector2>();
+
+
 
                 RectTransformUtility.ScreenPointToLocalPointInRectangle(
                     _canvas.transform as RectTransform,
@@ -45,10 +86,9 @@ namespace Studio23.SS2.BetterCursor.Core
 
                 _cursorTransoform.localPosition = localMousePosition;
             }
-            else
-            {
-                Debug.LogError("Cursoractionasset not assigned");
-            }
+
+            Debug.Log("Is Dragging :" + isDragging);
         }
+
     }
 }
